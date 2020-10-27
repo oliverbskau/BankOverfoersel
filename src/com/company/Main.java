007154f00ba7ebf44ed7973d0297aa068ac68dc8
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-10-27 14:58:36 +0100

    menu med mere

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,40 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        //Test
+
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+        System.out.println("Er du oprættet i systemet?");
+        System.out.print("ja/nej:");
+        String jaNej = in.nextLine();
+
+        switch(jaNej){
+
+            case "ja":
+                menu();
+                break;
+
+            case "nej":
+                System.out.print("\nIndtast fulde navn: ");
+                Account.setOwnerName() = in.nextLine();
+                break;
+
+
+        }
 
     }
+
+    public static void menu(){
+
+        System.out.println("Menu:");
+        System.out.println("1: Indæt penge på din konto");
+        System.out.println("2. Hæv penge fra din konto");
+        System.out.println("3. Overfør penge til en anden konto");
+
+    }
+
 }
