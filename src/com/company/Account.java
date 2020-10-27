5dc02e1b0b9aac3415470b11436c557ee1a1174d
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2020-10-27 14:37:10 +0100

    Opretter get og setter for accountNumber

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -6,7 +6,16 @@ public class Account {
     String ownerName;
     double balance;
 
+    public Account(String accountNumber, String ownerName){
+        this.ownerName = ownerName;
+        this.accountNumber = accountNumber;
+    }
 
+    public String getAccountNumber(String accountNumber) {
+        return accountNumber;
+    }
 
-
+    public void setAccountnumber(String newaccountNumber){
+        this.accountNumber = newaccountNumber;
+    }
 }
