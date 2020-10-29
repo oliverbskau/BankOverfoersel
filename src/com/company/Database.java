1a751546e98dfdf8536559da0b4f764652ce0c3e
Andreas-Hjort <73465555+Andreas-Hjort@users.noreply.github.com>
2020-10-29 11:53:36 +0100

    Ændrede int til double

--- src/com/company/Database.java
+++ src/com/company/Database.java
@@ -5,8 +5,9 @@ public class Database {
 
     private ArrayList<Account> database = new ArrayList<Account>();
 
-    public void add(String ownerName, String accountNumber, int balance){
+    public void add(String ownerName, String accountNumber,double balance){
         Account account = new Account(ownerName, accountNumber, balance);
         database.add(account);
     }
+
 }
