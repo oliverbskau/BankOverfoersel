88e9e9e5243b0fc492519a08679266924ba56211
oliverbskau <48017544+oliverbskau@users.noreply.github.com>
2020-10-29 12:20:49 +0100

    menu loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,23 +10,22 @@ public class Main {
     public double balance;
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
         Main main = new Main();
         main.startProgram();
     }
 
-    public void startProgram(){
+    public void startProgram() {
         Scanner in = new Scanner(System.in);
-        int choice = 0;
         System.out.println("Er du oprættet i systemet?");
         System.out.print("ja/nej: ");
         String jaNej = in.nextLine();
 
-        switch(jaNej){
+        switch (jaNej) {
 
             case "ja":
-                menu();
+                menu(ownerName,accountNumber,balance);
                 break;
 
             case "nej":
@@ -35,42 +34,46 @@ public class Main {
                 System.out.print("Indtast dit ønskede account nummer: ");
                 accountNumber = in.nextLine();
                 Random random = new Random();
-                balance = 1000+random.nextInt(4000);
+                balance = 1000 + random.nextInt(4000);
                 database.add(ownerName, accountNumber, balance);
-                menu();
-                break;
-        }
-
-        choice = in.nextInt();
-        switch(choice){
-
-            case 1:
-                System.out.println("Indæt penge på din konto");
-
-
-                break;
-
-            case 2:
-                System.out.println("Hæv penge fra din konto");
-                System.out.println("Din saldo: " + balance);
-
-                break;
-
-            case 3:
-                System.out.println("Overfør penge til en anden konto");
-
+                menu(ownerName,accountNumber,balance);
                 break;
         }
     }
 
-    public static void menu(){
+    public static void menu(String ownerName, String accountNumber, double balance) {
 
         System.out.println("\nMenu:");
         System.out.println("1: Indæt penge på din konto");
         System.out.println("2. Hæv penge fra din konto");
         System.out.println("3. Overfør penge til en anden konto");
+        System.out.println("9. Afslut program");
         System.out.print("Indtast tal: ");
 
-    }
+        Scanner in = new Scanner(System.in);
+        int choice = 0;
+        choice = in.nextInt();
+        switch (choice) {
 
+            case 1:
+                System.out.println("Indæt penge på din konto");
+
+                menu(ownerName,accountNumber,balance);
+                break;
+            case 2:
+                System.out.println("Hæv penge fra din konto");
+                System.out.println("Din saldo: " + balance);
+
+                menu(ownerName,accountNumber,balance);
+                break;
+            case 3:
+                System.out.println("Overfør penge til en anden konto");
+
+                menu(ownerName,accountNumber,balance);
+                break;
+            case 9:
+                System.exit(0);
+        }
+    }
 }
+
